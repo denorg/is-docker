9e6b21600e59a752d0956e68f9528d9402a46c48
Gabriel Francisco <gabrinelson27@gmail.com>
2020-08-25 16:40:12 -0300

    Remove unnecessary function

--- cli.ts
+++ cli.ts
@@ -1,11 +1,7 @@
 import { isDocker } from "./mod.ts";
 
-const cli = async () => {
-  if (await isDocker()) {
-  console.log('You are inside docker environment!');
-  } else {
+if (await isDocker()) {
+    console.log('You are inside docker environment!');
+} else {
     console.log('You are not in docker environment!');
-  }
 }
-
-cli();
