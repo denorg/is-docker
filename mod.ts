63ea59012ebf74aa689a862dd764ba3bf6ac1832
Thomas <thomas@hexf.me>
2021-04-25 23:57:34 +1200

    Remove async on functions which dont await

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 let hasDocker: boolean;
 
-const hasDockerEnv = async () => {
+const hasDockerEnv = () => {
 	try {
 		Deno.statSync('/.dockerenv');
 		return true;
@@ -9,18 +9,18 @@ const hasDockerEnv = async () => {
 	}
 }
 
-const hasDockerCGroup = async () => {
+const hasDockerCGroup = () => {
 	try {
-    const data = Deno.readFileSync("/proc/self/cgroup");
+		const data = Deno.readFileSync("/proc/self/cgroup");
 		return new TextDecoder("utf-8").decode(data).includes('docker')
 	} catch (_) {
 		return false;
 	}
 }
 
-export async function isDocker() {
+export function isDocker() {
   if (hasDocker === undefined) {
-	  hasDocker = await hasDockerEnv() || await hasDockerCGroup();
+	  hasDocker = hasDockerEnv() || hasDockerCGroup();
   }
   return hasDocker;
 }
